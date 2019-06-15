edfa95661d501cc22fae041c029986ec78d25d63
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-15 16:47:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+function justInvoke(func){
+  return func();
+}
+
+function setThisWithCall(func, name, arg){
+  return func.call(name, arg);
+}
+
+function setThisWithApply(func, name, args){
+  return func.apply(name, args);
+}
+
+function returnNewFunctionOf(func, name, args){
+  return func.bind(name, args);
+}
